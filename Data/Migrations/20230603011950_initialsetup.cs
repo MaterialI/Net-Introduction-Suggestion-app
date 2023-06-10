e08d83680d766030fa65f00a9eb035ae255fc134
MaterialI <94946672+MaterialI@users.noreply.github.com>
2023-06-10 11:49:31 -0700

    the main and first commit

---
+++ Data/Migrations/20230603011950_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuggestionApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suggestion",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SuggestionQuestion = table.Column<string>(nullable: true),
+                    SuggestionAnswer = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suggestion", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suggestion");
+        }
+    }
+}
---
+++ Models/Suggestion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuggestionApp.Models
+{
+    public class Suggestion
+    {
+        public int Id { get; set; }
+        public string SuggestionQuestion { get; set; }
+        public string SuggestionAnswer { get; set; }
+        public Suggestion()
+        {
+
+        }
+    }
+}
